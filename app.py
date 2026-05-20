edaba25fc72e0ca2620e3241f02e2139fae6c053
Srishti Srishti <srishti218@gmail.com>
2026-05-20 18:26:16 +0530

    app.py modified

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def home():
 def lumeofilters():
     return render_template("lumeofilters.html")
 
+@app.route("/quickreels.html")
+def lumeofilters():
+    return render_template("quickreels.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
