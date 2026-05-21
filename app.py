3cee2cb17badf245c36b92b29913f7818cc65d7b
Srishti Srishti <srishti218@gmail.com>
2026-05-21 16:26:50 +0530

    voicent.html added

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def quickreels():
 def scantextpdf():
     return render_template("scantextpdf.html")
 
+@app.route("/voicent.html")
+def voicent():
+    return render_template("voicent.html")
 
 
 if __name__ == "__main__":
